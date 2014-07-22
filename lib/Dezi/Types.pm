32387fd291f9965bf648d5dea9c62e758b9872a3
Peter Karman <karpet@peknet.com>
2014-07-22 13:22:20 -0500

    small optimization on coercion (via tobyink)

--- lib/Dezi/Types.pm
+++ lib/Dezi/Types.pm
@@ -50,7 +50,7 @@ coerce DeziFileRules, from ArrayRef, via { File::Rules->new($_) };
 
 # URI (coerce to Str)
 declare DeziUriStr, as Str;
-coerce DeziUriStr, from Object, via {"$_"};
+coerce DeziUriStr, from Object, q{"$_"};
 
 # Epoch
 declare DeziEpoch, as Maybe [Int];
